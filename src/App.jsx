71ab6acf6831f17b0413042333fb4ffec08376cc
bkornusov <batyrkornusov@gmail.com>
2025-03-16 17:06:56 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ReactSplit, { SplitDirection } from "@devbookhq/splitter";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <ReactSplit
+        SplitDirection={SplitDirection.Horizontal}
+        minWidths={[200, 400]}
+        gutterClassName="custom-gutter-horizontal"
+        draggerClassName="custom-dragger-horizontal"
+      >
+        <div className="initiative-panel" style={{ background: "beige" }}>
+          <h1>Initiative Panel</h1>
+        </div>
+        <div className="sheet-panel" style={{ background: "lightblue" }}>
+          <h1>Sheet Panel</h1>
+        </div>
+      </ReactSplit>
+    </>
+  );
+}
+
+export default App;
+
+// <div className="vite-react">
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src={viteLogo} className="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://react.dev" target="_blank">
+//       <img src={reactLogo} className="logo react" alt="React logo" />
+//     </a>
+//   </div>
+//   <h1>Vite + React</h1>
+//   <div className="card">
+//     <button onClick={() => setCount((count) => count + 1)}>
+//       count is {count}
+//     </button>
+//     <p>
+//       Edit <code>src/App.jsx</code> and save to test HMR
+//     </p>
+//   </div>
+//   <p className="read-the-docs">
+//     Click on the Vite and React logos to learn more
+//   </p>
+// </div>;
